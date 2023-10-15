287075e4a78295a707223bf30b12b421efcaa75d
Shabab Noor <ashababnoor@gmail.com>
2023-10-15 13:44:27 +0600

    added colors in output messages and exit message in math-quiz

--- miscellaneous/math-quiz.py
+++ miscellaneous/math-quiz.py
@@ -5,6 +5,28 @@ number_lower_range = 1
 number_upper_range = 10
 question_template = "What is {number1} {operand} {number2}?"
 
+
+# Define bold text and reset color and formatting
+bold='\033[1m'  # bold text
+reset='\033[0m' # reset color and formatting
+
+# Define color codes (non-bold)
+blue='\033[0;34m'       # Blue
+light_blue='\033[0;36m' # Light Blue
+green='\033[0;32m'      # Green
+red='\033[0;31m'        # Red
+yellow='\033[0;33m'     # Yellow
+purple='\033[0;35m'     # Purple
+
+# Define bold color codes
+blue_bold='\033[1;34m'       # Bold Blue
+light_blue_bold='\033[1;36m' # Bold Light Blue
+green_bold='\033[1;32m'      # Bold Green
+red_bold='\033[1;31m'        # Bold Red
+yellow_bold='\033[1;33m'     # Bold Yellow
+purple_bold='\033[1;35m'     # Bold Purple
+
+
 # Custom Function
 std_print = print
 
@@ -19,7 +41,7 @@ def custom_print(message: str) -> None:
         None
     '''
     custom_print_prefix = "math-quiz >>"
-    std_print(f"{custom_print_prefix} {message}")
+    std_print(f"{blue_bold}{custom_print_prefix}{reset} {message}")
 
 print = custom_print
 
@@ -62,6 +84,7 @@ def get_answer(answer: float) -> bool:
     
     if user_wants_to_exit:
         keep_playing = False
+        print_exit_message()
         return keep_playing
     
     if user_input == answer:
@@ -92,10 +115,13 @@ def check_for_exit_statement() -> tuple[bool, float]:
     return False, float(user_input)
 
 def print_wrong_answer_message() -> None:
-    print("Wrong Answer! Try again.")
+    print(f"{red}Wrong Answer!{reset} Try again.")
 
 def print_correct_answer_message() -> None:
-    print("Correct answer!")
+    print(f"{green}Correct answer!{reset}")
+
+def print_exit_message() -> None:
+    print(f"{light_blue}Good bye!{reset}")
 
 # Main driver code
 if __name__ == "__main__":
