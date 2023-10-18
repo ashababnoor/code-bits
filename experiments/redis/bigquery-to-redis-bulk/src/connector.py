76c452b7e3d8f8ec650df7ab06df90e2e50e3aef
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 16:34:50 +0600

    added bigquery class and checked connectivity; updated gitignore

--- experiments/redis/bigquery-to-redis-bulk/src/connector.py
+++ experiments/redis/bigquery-to-redis-bulk/src/connector.py
@@ -1,7 +1,12 @@
 import redis
+from loader import Bigquery
 
 r = redis.Redis(host="localhost", port=6379)
-ping_output = r.ping()
+bq = Bigquery(google_cred=None)
 
 if __name__ == "__main__":
-    print(f"Ping successful: {ping_output}")
+    redis_connected = r.ping()
+    print(f"Connection to Redis successful: {redis_connected}")
+    
+    bq_connected = bq.test_connection()
+    print(f"Connection to BigQuery successful: {bq_connected}")
---
+++ experiments/redis/bigquery-to-redis-bulk/src/loader.py
@@ -0,0 +1,22 @@
+from google.cloud import bigquery
+
+class Bigquery:
+    def __init__(self, google_cred):
+        self.__cnn = Bigquery._get_connection(google_cred)
+
+    def execute(self, query):
+        for row in self.__cnn.query(query):
+            yield row
+    
+    def test_connection(self):
+        import os
+        datasets = self.__cnn.list_datasets(project=os.environ.get("BIGQUERY_PROJECT_ID"))
+        return True if datasets else False
+
+    @staticmethod
+    def _get_connection(cred):
+        if cred is not None:
+            import os
+
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred
+        return bigquery.Client()
