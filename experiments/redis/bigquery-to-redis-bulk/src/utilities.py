06cdcc92519bb1b8f5702de9b80bc564691f1a71
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 18:44:42 +0600

    added driver code to utilities in bq to redis ingestor

--- experiments/redis/bigquery-to-redis-bulk/src/utilities.py
+++ experiments/redis/bigquery-to-redis-bulk/src/utilities.py
@@ -21,3 +21,33 @@ def get_dunder_methods(object: any) -> list:
         if callable(getattr(object, method)) 
         and method.startswith('__')
     ]
+
+if __name__ == "__main__":
+    from connector import bq
+    import os
+    
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    query_dir = "sql/"
+    query_file_name = "address_history" # "popular_search_terms"
+    query_file_ext = "sql"
+
+    query_file_path = os.path.join(
+        root_dir,
+        query_dir, 
+        f"{query_file_name}.{query_file_ext}"
+    )
+    with open(query_file_path, "r") as file:
+        query = file.read()
+
+    for row in bq.query(query):
+        attributes = get_attributes(row)
+        regular_methods = get_regular_methods(row)
+        dunder_methods = get_dunder_methods(row)
+        
+        print(f"{attributes = }")       
+        print(f"{regular_methods = }") 
+
+        print(f"{row.items() = }")
+        print(f"{row.keys() = }")
+        print(f"{row.values() = }")
+        break
