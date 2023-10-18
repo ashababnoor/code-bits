808a3bde60296934b0582397b8a755653d8451df
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 15:34:31 +0600

    added connector and popular search terms query in bq to redis bulk ingestor

---
+++ experiments/redis/bigquery-to-redis-bulk/sql/popular_search_terms.sql
@@ -0,0 +1,30 @@
+with base_intl_top_terms as (
+  select *
+  from `bigquery-public-data.google_trends.international_top_terms`
+  qualify 
+    row_number() 
+  over( 
+    partition by 
+      country_code
+      , week
+      , rank
+    order by 
+      refresh_date
+  ) = 1
+)
+
+select
+  country_code
+  , week
+  , array_agg(
+    struct(
+      term as term
+      , rank as rank
+      , score as score
+    )
+    ignore nulls order by rank asc
+  ) as popular_terms
+from base_intl_top_terms
+group by
+  country_code
+  , week
