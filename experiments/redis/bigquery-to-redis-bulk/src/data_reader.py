0bb15a02a93888b0d38c6cbce5f9b291eb98fbcb
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 17:07:21 +0600

    added data reader in bq-to-redis-bulk that reads query and writes output to text file

--- experiments/redis/bigquery-to-redis-bulk/src/data_reader.py
+++ experiments/redis/bigquery-to-redis-bulk/src/data_reader.py
@@ -11,7 +11,17 @@ query_output_file_name = "query_output.txt"
 
 with open(os.path.join(root_dir, query_dir, query_file_name), "r") as file:
     query = file.read()
+    print("Query reading successful.")
 
+query_executed_successfully = True
 
 with open(os.path.join(root_dir, data_dir, query_output_file_name), "w") as file:
-    pass
+    for row in bq.execute(query=query):
+        if query_executed_successfully:
+            print("Query execution successful.")
+            query_executed_successfully = False
+        
+        file.write(str(row))
+    
+    print("Query output writing to successful.")
+    print(f"Output: {os.path.join(root_dir, data_dir, query_output_file_name)}")
