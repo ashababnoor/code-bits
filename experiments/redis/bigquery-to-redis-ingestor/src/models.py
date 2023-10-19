158a48962004a5531dcd29a008f2fbaa843e08ca
Shabab Noor <ashababnoor@gmail.com>
2023-10-19 17:16:53 +0600

    added new method in query object that adds limits to queries

--- experiments/redis/bigquery-to-redis-ingestor/src/models.py
+++ experiments/redis/bigquery-to-redis-ingestor/src/models.py
@@ -1,4 +1,5 @@
 import os
+from copy import copy 
 
 
 class Query:
@@ -23,13 +24,34 @@ class Query:
     def __set_query_string(self) -> str:
         with open(self.__query_path, "r") as query_file:
             self.__query_string = query_file.read()
+            
+    def __update_query_string(self, query_string) -> str:
+        self.__query_string = query_string
 
     def get_query_string(self):
         return self.__query_string
+    
+    def add_limit(self, limit: int, inplace=False) -> 'Query':
+        if type(limit) != int:
+            raise Exception("Limit must be an integer.")
+        limit_added_query_string = f"select * from ({self.get_query_string()}) limit {limit}"
+        
+        if inplace:
+            self.__update_query_string(limit_added_query_string)
+            return self
+
+        query = copy(self)
+        query.__update_query_string(limit_added_query_string)
+        return query
 
 
 if __name__ == "__main__":
     query = Query("popular_search_terms")
 
     print(f"{query.query_name = }")
+    
+    print("\nOriginal query:")
     print(query.get_query_string())
+    
+    print("\nQuery after adding limit:")
+    print(query.add_limit(10).get_query_string())
