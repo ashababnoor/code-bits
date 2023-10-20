328dd0505e710ba7934d5206bfef5dca130fda0f
Shabab Noor <ashababnoor@gmail.com>
2023-10-20 23:26:47 +0600

    added write_to_text_file function in Bigquery class in bq to redis ingestor

--- experiments/redis/bigquery-to-redis-ingestor/src/loader.py
+++ experiments/redis/bigquery-to-redis-ingestor/src/loader.py
@@ -1,4 +1,6 @@
 from google.cloud import bigquery
+from models import Query
+from utilities import Color, now
 
 
 class Bigquery:
@@ -24,3 +26,22 @@ class Bigquery:
             project=os.environ.get("BIGQUERY_PROJECT_ID")
         )
         return True if datasets else False
+        
+    def write_to_text_file(
+            self, 
+            query: Query, 
+            save_path: str, 
+            benchmark: bool=False, 
+            verbose: bool=False
+        ):
+        import timeit
+        
+        if verbose: print(f"{Color.light_blue}Log:{Color.reset} Writing to file started: {now()}")
+        
+        with open(save_path, "w") as file:
+            for row in self.execute(query.get_query_string()):
+                file.write(str(dict(row))+"\n")
+        
+        if verbose: print(f"{Color.light_blue}Log:{Color.reset} Writing to file ended: {now()}")
+        print("Writing to file successful!")
+        if verbose: print("")
