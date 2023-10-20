db41ac96312f1b37e1e9daef038e1183799e0c90
Shabab Noor <ashababnoor@gmail.com>
2023-10-20 19:54:43 +0600

    moved colors to utility class Color in bq to redis ingestor

--- experiments/redis/bigquery-to-redis-ingestor/src/utilities.py
+++ experiments/redis/bigquery-to-redis-ingestor/src/utilities.py
@@ -1,29 +1,35 @@
 from datetime import datetime
+from dataclasses import dataclass
+
+# Utility classes
+@dataclass
+class Color:
+    # Define bold text and reset color and formatting
+    bold: str = '\033[1m'               # bold text
+    reset: str = '\033[0m'              # reset color and formatting
+
+    # Define non-bold color codes
+    blue: str = '\033[0;34m'            # Blue
+    light_blue: str = '\033[0;36m'      # Light Blue
+    green: str = '\033[0;32m'           # Green
+    red: str = '\033[0;31m'             # Red
+    yellow: str = '\033[0;33m'          # Yellow
+    purple: str = '\033[0;35m'          # Purple
+
+    # Define bold color codes
+    blue_bold: str = '\033[1;34m'       # Bold Blue
+    light_blue_bold: str = '\033[1;36m' # Bold Light Blue
+    green_bold: str = '\033[1;32m'      # Bold Green
+    red_bold: str = '\033[1;31m'        # Bold Red
+    yellow_bold: str = '\033[1;33m'     # Bold Yellow
+    purple_bold: str = '\033[1;35m'     # Bold Purple
 
 
-# Define bold text and reset color and formatting
-bold='\033[1m'               # bold text
-reset='\033[0m'              # reset color and formatting
-
-# Define non-bold color codes
-blue='\033[0;34m'            # Blue
-light_blue='\033[0;36m'      # Light Blue
-green='\033[0;32m'           # Green
-red='\033[0;31m'             # Red
-yellow='\033[0;33m'          # Yellow
-purple='\033[0;35m'          # Purple
-
-# Define bold color codes
-blue_bold='\033[1;34m'       # Bold Blue
-light_blue_bold='\033[1;36m' # Bold Light Blue
-green_bold='\033[1;32m'      # Bold Green
-red_bold='\033[1;31m'        # Bold Red
-yellow_bold='\033[1;33m'     # Bold Yellow
-purple_bold='\033[1;35m'     # Bold Purple
-
-
+# Utility lambdas
 now: str = lambda: datetime.now().strftime('%H:%M:%S')
 
+
+# Utility functions
 def get_attributes(object: any) -> list:
     return [
         attr 
@@ -37,7 +43,7 @@ def get_regular_methods(object: any) -> list:
         for method in dir(object) 
         if callable(getattr(object, method)) and not method.startswith('__')
     ]
-    
+
 def get_dunder_methods(object: any) -> list:
     return [
         method 
@@ -46,6 +52,7 @@ def get_dunder_methods(object: any) -> list:
     ]    
 
 
+# Driver code
 if __name__ == "__main__":
     from connector import bq
     from models import Query
