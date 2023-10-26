c939bf6b1ca9dd8ca0be20fe9e3449f86f351fb8
Shabab Noor <ashababnoor@gmail.com>
2023-10-26 23:46:29 +0600

    added RedisIngestor class in bq<>redis ingestor

---
+++ experiments/redis/bigquery-to-redis-ingestor/src/ingestor.py
@@ -0,0 +1,11 @@
+import redis
+from models import Query
+from loader import Bigquery
+from utilities import Print
+from tqdm import tqdm
+
+
+class RedisIngestor:
+    def __init__():
+        pass
+    
