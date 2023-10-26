67ab7387990126f42b973b9d58f8c0b77a0f9ae6
Shabab Noor <ashababnoor@gmail.com>
2023-10-26 22:32:30 +0600

    code cleaning and refactoring in progressbar.sh

--- shell-scripts/progressbar.sh
+++ shell-scripts/progressbar.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-
 # Function to draw the progress bar
 draw_progress_bar() {
-    local progress=$(( $1 * 1 ))  # Scale the progress to fit the bar width
+    local progress_bar_width=$(( $1 * 1 ))  # Scale the progress to fit the bar width
     local bar=""
+    local current_progress=$1
 
     # Create the progress bar
-    for (( i = 0; i < $progress; i++ )); do
+    for (( i = 0; i < $progress_bar_width; i++ )); do
         bar+="="
     done
 
-    printf "[%-50s] %d%%" "$bar" "$1"
+    printf "[%-50s] %d%%" "$bar" "$current_progress"
 }
+
 
 # Loop to simulate progress
 for (( i = 0; i <= 50; i++ )); do
