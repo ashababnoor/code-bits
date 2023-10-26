eb25c7b660ca321756f9ebb0d1b1ecdffc791b01
Shabab Noor <ashababnoor@gmail.com>
2023-10-26 23:40:36 +0600

    wrapped progressbar creation logic in a separate function to make it callable in otner places in progressbar.sh

--- shell-scripts/progressbar.sh
+++ shell-scripts/progressbar.sh
@@ -23,6 +23,28 @@ draw_progress_bar() {
     printf "${message}[%-${width}s] %d%%" "$progress_bar" "$percentage"
 }
 
+function progressbar() {
+    default_total_iterations=100
+    total_iterations=${1-$default_total_iterations}
+
+    default_progress_bar_width=$(( $(tput cols) - 20 ))
+    progress_bar_width=${2-$default_progress_bar_width}
+
+    default_progress_bar_message=""
+    progress_bar_message=${3-$default_progress_bar_message}
+
+    # Loop to simulate progress
+    for (( iteration = 0; iteration <= $total_iterations; iteration++ )); do
+        draw_progress_bar $iteration $total_iterations $progress_bar_width $progress_bar_message
+
+        sleep 0.05     # Simulate some work being done
+        printf "\r"    # Move the cursor back to the beginning of the line
+    done
+
+    echo ""  # Move to the next line after the progress bar is complete
+}
+
+
 default_total_iterations=100
 total_iterations=${1-$default_total_iterations}
 
@@ -32,12 +54,4 @@ progress_bar_width=${2-$default_progress_bar_width}
 default_progress_bar_message=""
 progress_bar_message=${3-$default_progress_bar_message}
 
-# Loop to simulate progress
-for (( iteration = 0; iteration <= $total_iterations; iteration++ )); do
-    draw_progress_bar $iteration $total_iterations $progress_bar_width $progress_bar_message
-
-    sleep 0.05     # Simulate some work being done
-    printf "\r"    # Move the cursor back to the beginning of the line
-done
-
-echo ""  # Move to the next line after the progress bar is complete
+progressbar $total_iterations $progress_bar_width $progress_bar_message
