73cf2ea7d60692910dc37164c1914b86c360fc43
Shabab Noor <ashababnoor@gmail.com>
2023-10-26 22:55:58 +0600

    added variable length progressbar creating capability in progressbar.sh

--- shell-scripts/progressbar.sh
+++ shell-scripts/progressbar.sh
@@ -1,6 +1,7 @@
 # Function to draw the progress bar
 draw_progress_bar() {
-    local current_progress=$(( $1 * 1 ))  # Scale the progress to fit the bar width
+    local current_progress=$(( $1 * 1 ))
+    local total_width=$2
     local bar=""
 
     # Create the progress bar
@@ -8,15 +9,17 @@ draw_progress_bar() {
         bar+="="
     done
 
-    printf "[%-50s] %d%%" "$bar" "$current_progress"
+    printf "[%-${total_width}s] %d%%" "$bar" "$current_progress"
 }
 
+progress_bar_total_width_default_value=50
+progress_bar_total_width=${1-$progress_bar_total_width_default_value}
 
 # Loop to simulate progress
-for (( i = 0; i <= 50; i++ )); do
-    draw_progress_bar $i
-    sleep 0.1  # Simulate some work being done
-    printf "\r"  # Move the cursor back to the beginning of the line
+for (( i = 0; i <= $progress_bar_total_width; i++ )); do
+    draw_progress_bar $i $progress_bar_total_width
+    sleep 0.05     # Simulate some work being done
+    printf "\r"    # Move the cursor back to the beginning of the line
 done
 
 echo ""  # Move to the next line after the progress bar is complete
