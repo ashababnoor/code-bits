00dd3dba2785308fd9e5d7519a2c4a2544d98881
Shabab Noor <ashababnoor@gmail.com>
2023-10-27 22:48:10 +0600

    added utility classes and functions testbench in py-utils/utilities

--- projects/tools/py-utils/utilities/__main__.py
+++ projects/tools/py-utils/utilities/__main__.py
@@ -6,4 +6,38 @@ from functions import *
 from lambdas import *
 
 
-print("Hello world!")
+if __name__ == "__main__":
+    
+    with CodeBlock("Utility functions") as block:
+        color = Color()
+        print(f"{get_attributes(color) = }")       
+        print(f"{get_regular_methods(color) = }") 
+    
+    with CodeBlock("Utility lambdas") as block:
+        print(f"{now() = }")
+    
+    with CodeBlock("Utility classes") as block:
+        Print.info("Info message using Print class")
+        Print.log("log message using Print class")
+        Print.success("success message using Print class")
+        Print.warning("warning message using Print class")
+        Print.error("error message using Print class")
+        Print.keyword("Custom keyword", "Keyword message using Print class")
+    
+    with TimerBlock(force_record=True) as block:
+        start = 1
+        finish = 1_000
+        print(f"Generating list from {start:,} to {finish:,}")
+        list_ = [i for i in range(start, finish+1)]
+    
+    with TimerBlock(force_record=True) as block:
+        start = 1
+        finish = 1_000
+        print(f"Generating list from {start:,} to {finish:,}")
+        list_ = [i for i in range(start, finish+1)]
+    
+    TimerBlock.timing_summary()
+    print()
+
+    with CodeBlock("Color Utility Class") as _:
+        Color.show_colors()
