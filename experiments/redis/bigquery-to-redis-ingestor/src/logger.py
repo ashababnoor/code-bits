2139901b7f11361786abfadc45e1d4b0f18e2724
Shabab Noor <ashababnoor@gmail.com>
2023-10-29 05:10:11 +0600

    added logger in bq<>redis ingestor

---
+++ experiments/redis/bigquery-to-redis-ingestor/src/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+logging_level = logging.DEBUG
+logging_file = ".log"
+
+logging.basicConfig(
+    level=logging_level,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s [%(levelname)s]: %(message)s',
+    datefmt="%a %Y-%m-%d %H:%M:%S %Z %z",
+    handlers=[
+        logging.FileHandler(logging_file),  # Save log messages to a file
+        logging.StreamHandler()             # Print log messages to the console
+    ]
+)
+
+logger = logging.getLogger(__name__)
