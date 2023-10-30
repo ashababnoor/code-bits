ecd80d116407e5c94078f898749002ae96417ef0
Shabab Noor <ashababnoor@gmail.com>
2023-10-31 02:28:28 +0600

    added collapse_separation argument in CodeBlock to make it optional but true by default in py-utils

--- projects/tools/py-utils/utilities/classes/codeblock.py
+++ projects/tools/py-utils/utilities/classes/codeblock.py
@@ -12,7 +12,8 @@ class CodeBlock():
             pretty: bool=False,
             add_separation: bool=True,
             top_separation: int=1,
-            bottom_separation: int=1
+            bottom_separation: int=1,
+            collapse_separation: bool=True,
         ):
         if message is not None:
             message = str(message).strip()
@@ -26,26 +27,36 @@ class CodeBlock():
         self.add_separation = add_separation
         self.top_separation = top_separation
         self.bottom_separation = bottom_separation
+        self.collapse_separation = collapse_separation
     
     @staticmethod
     def check_global_last_codeblock_bottom_separation():
         global global_last_codeblock_has_bottom_separation
         global global_last_codeblock_bottom_separation_val
+        
         if "global_last_codeblock_has_bottom_separation" not in globals() or not isinstance(global_last_codeblock_has_bottom_separation, bool):
             global_last_codeblock_has_bottom_separation = False
+        
         if "global_last_codeblock_bottom_separation_val" not in globals() or not isinstance(global_last_codeblock_bottom_separation_val, int):
             global_last_codeblock_bottom_separation_val = 0
+        
         return global_last_codeblock_has_bottom_separation, global_last_codeblock_bottom_separation_val
     
     def __enter__(self):
-        last_codeblock_has_bottom_separation, last_codeblock_has_bottom_separation = CodeBlock.check_global_last_codeblock_bottom_separation()
-
-        if self.add_separation and isinstance(self.top_separation, int) and self.top_separation > 0:
+        if (
+            self.add_separation 
+            and isinstance(self.top_separation, int) 
+            and self.top_separation > 0
+        ):
+            last_codeblock_has_bottom_separation, last_codeblock_has_bottom_separation = CodeBlock.check_global_last_codeblock_bottom_separation()
             effective_top_separation = self.top_separation
-            if last_codeblock_has_bottom_separation:
-                if self.top_separation >= last_codeblock_has_bottom_separation:
-                    effective_top_separation = self.top_separation - last_codeblock_has_bottom_separation
-                    
+            if (
+                self.collapse_separation 
+                and last_codeblock_has_bottom_separation
+                and self.top_separation >= last_codeblock_has_bottom_separation
+            ):
+                effective_top_separation = self.top_separation - last_codeblock_has_bottom_separation
+            
             print("\n" * (effective_top_separation), end='')
         
         if self.message is None and not self.declarative:
@@ -65,12 +76,16 @@ class CodeBlock():
             print(horizontal_line)
         
         return self
-
+    
     def __exit__(self, exc_type, exc_value, traceback):
         global global_last_codeblock_has_bottom_separation
         global global_last_codeblock_bottom_separation_val
         
-        if self.add_separation and isinstance(self.bottom_separation, int) and self.bottom_separation > 0:
+        if (
+            self.add_separation 
+            and isinstance(self.bottom_separation, int) 
+            and self.bottom_separation > 0
+        ):
             print("\n" * (self.bottom_separation), end='') 
             global_last_codeblock_has_bottom_separation = True
             global_last_codeblock_bottom_separation_val = self.bottom_separation
