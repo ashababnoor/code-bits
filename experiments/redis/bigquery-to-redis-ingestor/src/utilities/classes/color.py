eea54b931fe47ea11b1f4f800da10e48af282a75
Shabab Noor <ashababnoor@gmail.com>
2023-10-30 23:57:28 +0600

    added chartreuse in Colors in bq<>redis ingestor

--- experiments/redis/bigquery-to-redis-ingestor/src/utilities/classes/color.py
+++ experiments/redis/bigquery-to-redis-ingestor/src/utilities/classes/color.py
@@ -39,6 +39,7 @@ class Color:
     orange_red: str = '\033[38;5;202m'            # Orange Red
     light_sea_green: str = '\033[38;5;37m'        # Light Sea Green
     dodger_blue: str = '\033[38;5;33m'            # Dodger Blue
+    chartreuse: str = '\033[38;5;76m'             # Chartreuse
 
     # Define 8-bit color bold codes
     orage_bold: str = '\033[1;38;5;214m'          # Bold Orange
