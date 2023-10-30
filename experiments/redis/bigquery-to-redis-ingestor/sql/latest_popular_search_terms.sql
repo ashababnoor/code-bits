22233905a4308b1bd6c0bbbf1f543cc9956899ec
Shabab Noor <ashababnoor@gmail.com>
2023-10-30 23:45:41 +0600

    refactored latest popular search terms query in bq<>redis ingestor

--- experiments/redis/bigquery-to-redis-ingestor/sql/latest_popular_search_terms.sql
+++ experiments/redis/bigquery-to-redis-ingestor/sql/latest_popular_search_terms.sql
@@ -3,28 +3,19 @@ WITH base_intl_top_terms AS (
   FROM `bigquery-public-data.google_trends.international_top_terms`
   QUALIFY ROW_NUMBER() 
     OVER (PARTITION BY country_code, rank ORDER BY refresh_date desc) = 1
-),
+)
 
-current_week_data AS (
+, current_week_data AS (
   SELECT *
   FROM base_intl_top_terms
-  WHERE EXTRACT(YEAR FROM refresh_date) = EXTRACT(YEAR FROM CURRENT_DATE())
+  WHERE 1 = 1
+    AND rank = 1
+    AND EXTRACT(YEAR FROM refresh_date) = EXTRACT(YEAR FROM CURRENT_DATE())
     AND EXTRACT(WEEK FROM refresh_date) = EXTRACT(WEEK FROM CURRENT_DATE())
-    and rank = 1
 )
 
 SELECT
-  country_code,
-  week,
-  term,
-  -- ARRAY_AGG(
-  --   STRUCT(
-  --     term AS term,
-  --     rank AS rank,
-  --     score AS score
-  --   ) 
-  --   ORDER BY rank ASC
-  -- ) AS popular_terms
+  country_code
+  , term
 FROM current_week_data
--- GROUP BY country_code, week
-ORDER BY country_code, week DESC;
+ORDER BY country_code
