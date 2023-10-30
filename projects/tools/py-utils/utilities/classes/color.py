9dcf124ddb811034fe706e44c2e1387a33dbf649
Shabab Noor <ashababnoor@gmail.com>
2023-10-31 00:14:58 +0600

    minor refactoring in Color class in py-utils

--- projects/tools/py-utils/utilities/classes/color.py
+++ projects/tools/py-utils/utilities/classes/color.py
@@ -40,14 +40,14 @@ class Color:
     light_sea_green: str = '\033[38;5;37m'        # Light Sea Green
     dodger_blue: str = '\033[38;5;33m'            # Dodger Blue
     chartreuse: str = '\033[38;5;76m'             # Chartreuse
-    
+
     # Define 8-bit color bold codes
     orage_bold: str = '\033[1;38;5;214m'          # Bold Orange
     dark_orange_bold: str = '\033[1;38;5;208m'    # Bold Dark Orange
     orange_red_bold: str = '\033[1;38;5;202m'     # Bold Orange Red
     light_sea_green_bold: str = '\033[1;38;5;37m' # Bold Light Sea Green
     dodger_blue_bold: str = '\033[1;38;5;33m'     # Bold Dodger Blue
-    chartreuse_bold: str = '\033[1;38;5;76m'        # Bold Chartreuse
+    chartreuse_bold: str = '\033[1;38;5;76m'      # Bold Chartreuse
     
     @staticmethod
     def show_colors():
