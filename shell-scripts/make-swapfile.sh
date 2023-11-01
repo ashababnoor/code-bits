a96c5fd433bf4211fee39060fcbf2f80ce0d5f5a
Shabab Noor <ashababnoor@gmail.com>
2023-11-01 22:53:43 +0600

    added OS type check and added functions for modularity in make-swapfile.sh

--- shell-scripts/make-swapfile.sh
+++ shell-scripts/make-swapfile.sh
@@ -3,18 +3,33 @@
 default_swapfile_size=1
 swapfile_size=${1-$default_swapfile_size}
 
-echo "Allocating ${swapfile_size}G memory for the swap file"
-sudo fallocate -l "$swapfile_size"G /swapfile
+function make_swapfile() {
+    local swapfile_size=$1
 
-sudo chmod 600 /swapfile
-ls -hl /swapfile
-sudo mkswap /swapfile
+    echo "Allocating ${swapfile_size}G memory for the swap file"
+    sudo fallocate -l "$swapfile_size"G /swapfile
 
-echo "Swapfile has been created. Activating swapfile"
-sudo swapon /swapfile
+    sudo chmod 600 /swapfile
+    ls -hl /swapfile
+    sudo mkswap /swapfile
 
-echo "Running 'swapon --show' to confirm the swap space is enabled"
-sudo swapon --show
+    echo "Swapfile has been created. Activating swapfile"
+    sudo swapon /swapfile
 
-echo "Running 'free -h' to confirm the amount of swap space available"
-free -h
+    echo "Running 'swapon --show' to confirm the swap space is enabled"
+    sudo swapon --show
+
+    echo "Running 'free -h' to confirm the amount of swap space available"
+    free -h
+}
+
+function main() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        make_swapfile $swapfile_size
+    else
+        echo "OS type ${OSTYPE} is not supported."
+        echo "Run this script in a linux-gnu machine to make swapfile."
+    fi
+}
+
+main
