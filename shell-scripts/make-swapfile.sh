41ca5031b3c96cde36b8830e309805342b6f89d0
Shabab Noor <ashababnoor@gmail.com>
2023-11-01 22:44:58 +0600

    added documentation in make-swapfile.sh

--- shell-scripts/make-swapfile.sh
+++ shell-scripts/make-swapfile.sh
@@ -3,10 +3,18 @@
 default_swapfile_size=1
 swapfile_size=${1-$default_swapfile_size}
 
+echo "Allocating ${swapfile_size}G memory for the swap file"
 sudo fallocate -l "$swapfile_size"G /swapfile
+
 sudo chmod 600 /swapfile
 ls -hl /swapfile
 sudo mkswap /swapfile
+
+echo "Swapfile has been created. Activating swapfile"
 sudo swapon /swapfile
+
+echo "Running 'swapon --show' to confirm the swap space is enabled"
 sudo swapon --show
+
+echo "Running 'free -h' to confirm the amount of swap space available"
 free -h
