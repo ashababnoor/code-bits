176303ec590afaf98690e9c2067a052d60b26f71
Shabab Noor <ashababnoor@gmail.com>
2023-11-02 23:10:50 +0600

    broken down installation steps into function in ctop-installer

--- shell-scripts/ctop-installer.sh
+++ shell-scripts/ctop-installer.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+function install_ctop_on_linux_gnu() {
     sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64 -O /usr/local/bin/ctop
     sudo chmod +x /usr/local/bin/ctop
+}
 
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+function install_ctop_on_macos() {
     brew install ctop
+}
 
-else
-    echo "OS type ${OSTYPE} is not supported."
+function main() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        install_ctop_on_linux_gnu
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        install_ctop_on_macos
+    else
+        echo "OS type ${OSTYPE} is not supported."
+    fi
+}
 
-fi
+main
