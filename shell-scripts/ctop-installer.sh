7e57d436ef1f69ba49de668f12c48fcad83585d0
Shabab Noor <ashababnoor@gmail.com>
2023-11-02 23:07:16 +0600

    updated ctop installer script

--- shell-scripts/ctop-installer.sh
+++ shell-scripts/ctop-installer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64 -O /usr/local/bin/ctop
-sudo chmod +x /usr/local/bin/ctop
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64 -O /usr/local/bin/ctop
+    sudo chmod +x /usr/local/bin/ctop
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install ctop
+
+else
+    echo "OS type ${OSTYPE} is not supported."
+
+fi
