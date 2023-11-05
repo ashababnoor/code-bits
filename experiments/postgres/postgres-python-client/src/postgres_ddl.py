dcf3ba222b8b3c8b77a840d7364d88aa733a2c0c
Shabab Noor <ashababnoor@gmail.com>
2023-11-05 23:08:28 +0600

    added postgres create table function in postgres_ddl in postgres python client experiment

---
+++ experiments/postgres/postgres-python-client/src/postgres_ddl.py
@@ -0,0 +1,51 @@
+from postgres_connector import pg_connection
+
+def create_table(pg_connection, table_name: str, columns: list[str], data_types: dict[str]={}, properties: dict[str]={}):
+        # Create a cursor object
+        cur = pg_connection.cursor()
+
+        # Define the SQL statement to create the table
+        
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} "
+        
+        create_table_query += "(\n"
+        for column in columns:
+            if column == columns[-1]:
+                create_table_query += f"\t {column} {data_types.get(column, '')} \n"
+            else:
+                create_table_query += f"\t {column} {data_types.get(column, '')}, \n"
+        create_table_query += ")"
+        
+        print("Query:")
+        print(create_table_query)
+        
+        # Execute the SQL statement
+        cur.execute(create_table_query)
+
+        # Commit the transaction
+        pg_connection.commit()
+
+        # Close the cursor and connection
+        cur.close()
+        pg_connection.close()
+
+        print("Table created successfully!")
+        
+
+
+def main():
+    table_name = "demo_table"
+    columns = ["id", "name", "age"]
+    data_types = dict(
+        id="int",
+        name="varchar(50)",
+        age="float", 
+    )
+    properties = dict(
+        id="primary key"
+    )
+
+    create_table(pg_connection, table_name=table_name, columns=columns, data_types=data_types, properties=properties)
+
+if __name__ == "__main__":
+    main()
