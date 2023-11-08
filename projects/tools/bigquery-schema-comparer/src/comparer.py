801b72de4e544b20bd922928c3a9fc7759445d4e
Shabab Noor <ashababnoor@gmail.com>
2023-11-08 23:55:23 +0600

    added app.py as main function caller in bq-schema-comparer

--- projects/tools/bigquery-schema-comparer/src/comparer.py
+++ projects/tools/bigquery-schema-comparer/src/comparer.py
@@ -17,20 +17,5 @@ def compare_schemas(schema1, schema2):
                 similar_columns.append((col1['name'], col2['name'], col1['type']))
             
 
-    return common_columns, similar_columns, schema1_columns
-
-
-schema2 = load_schema('schema1.json')
-schema1 = load_schema('schema2.json')
-
-common_columns, similar_columns, schema1_columns = compare_schemas(schema1, schema2)
-
-print("Common columns:")
-for col, type in common_columns:
-    print(f"{col}, {type}")
-
-print("\nSimilar columns:")
-for col1, col2, type in similar_columns:
-    # print(f"{col1} (in schema1) and {col2} (in schema2) have similar data types.")
-    print(f"{col1}, {col2}, {type}")
+    return common_columns, similar_columns
 
