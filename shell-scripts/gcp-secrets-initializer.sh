1d115f3a41f8e7d36423d906e95ba4face1c4587
Shabab Noor <ashababnoor@gmail.com>
2023-11-15 23:52:59 +0600

    added script to initialize gcp secrets

---
+++ shell-scripts/gcp-secrets-initializer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+service_account_file_name=""
+
+function set_google_app_cred() {
+    local path=$1
+    echo -n "Setting GOOGLE_APPLICATION_CREDENTIALS... "
+    export GOOGLE_APPLICATION_CREDENTIALS=$path
+    echo "Done"
+}
+
+function main() {
+    local default_service_account_file_path="$(pwd)/secrets/$service_account_file_name"
+
+    if [[ -f $default_service_account_file_name ]]; then
+        echo "Service account json file found in secrets directory."
+        
+        local path=$default_service_account_file_name
+        set_google_app_cred $path
+    else
+        echo "Service account json file NOT found in secrets directory."
+        echo "GOOGLE_APPLICATION_CREDENTIALS is NOT set"
+    fi
+}
+
+main
