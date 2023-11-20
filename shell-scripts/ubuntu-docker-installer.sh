4a14a8619b940de5b4d34dc57da89f7420fec4be
Shabab Noor <ashababnoor@gmail.com>
2023-11-20 23:38:56 +0600

    updated ubuntu docker installer script with steps taken from official documentation

--- shell-scripts/ubuntu-docker-installer.sh
+++ shell-scripts/ubuntu-docker-installer.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-password=$1
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
-sudo usermod -aG docker ${USER}
-echo $password | su - ${USER}
-sudo apt install -y docker-compose
+# Add the repository to Apt sources:
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-echo -e "\n"
-echo "Successfully installed Docker and Docker-Compose"
-echo "Run the following command to check docker service status"
-echo -e "\n"
-echo -e "\t sudo systemctl status docker"
-echo -e "\n"
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
