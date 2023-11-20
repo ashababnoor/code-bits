d18973930dd97392597052c92244ab183c35b8c3
Shabab Noor <ashababnoor@gmail.com>
2023-11-20 23:50:08 +0600

    added option for vm user in repo to gcp vm script

--- shell-scripts/repo-to-gcp-vm.sh
+++ shell-scripts/repo-to-gcp-vm.sh
@@ -4,15 +4,18 @@
 default_gcp_project_id=""    # Google cloud platform porject ID
 default_gcp_project_zone=""  # Google cloud platform porject zone
 default_gcp_vm_name=""       # Google cloud platform virtual machine name
+default_gcp_vm_user="$USER"  # Google cloud platform virtual machine user
 default_gcp_vm_path="~/"     # Google cloud platform virtual machine path where contents will be sent
 
 repository_name=$(basename "$(pwd)")
 gcp_project_id=${1-$default_gcp_project_id}
 gcp_project_zone=${2-$default_gcp_project_zone}
 gcp_vm_name=${3-$default_gcp_vm_name}
-gcp_vm_path=${4-$default_gcp_vm_path}
+gcp_vm_user=${4-$default_gcp_vm_user}
+gcp_vm_path=${5-$default_gcp_vm_path}
 
-alias gssh="gcloud compute ssh --project=$gcp_project_id --zone=$gcp_project_zone $gcp_vm_name"
+
+alias gssh="gcloud compute ssh --project=$gcp_project_id --zone=$gcp_project_zone $gcp_vm_user@$gcp_vm_name"
 
 function create_repo_zip() {
     current_dir=$(pwd)
@@ -60,7 +63,7 @@ function send_repo_zip_to_gcp_vm_and_unzip() {
     echo "Moved to $(pwd)"
 
     echo "Sending $repository_name.zip to GCP VM $gcp_vm_name"
-    gcloud compute scp --project=$gcp_project_id --zone=$gcp_project_zone $repository_name.zip $gcp_vm_name:$gcp_vm_path
+    gcloud compute scp --project=$gcp_project_id --zone=$gcp_project_zone $repository_name.zip $gcp_vm_user@$gcp_vm_name:$gcp_vm_path
     echo "File sent successfully"
 
     cd $current_dir
