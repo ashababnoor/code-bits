5a4aad5f48b006b21347d96d054f22d127c6f2a2
Shabab Noor <ashababnoor@gmail.com>
2023-11-24 22:57:59 +0600

    updated resize svg function with more user controls

--- projects/tools/svg-to-jpg-converter/resize_svg.py
+++ projects/tools/svg-to-jpg-converter/resize_svg.py
@@ -1,39 +1,19 @@
-import xml.dom.minidom
-import svgwrite
-
-def resize_svg(input_svg):
-    # Parse the SVG file to get width and height
-    doc = xml.dom.minidom.parse(input_svg)
-    svg_node = doc.getElementsByTagName('svg')[0]
-    original_width = svg_node.getAttribute('width')
-    original_height = svg_node.getAttribute('height')
-
-    # Remove units from width and height attributes
-    original_width = int(original_width.replace('px', '').replace('%', ''))
-    original_height = int(original_height.replace('px', '').replace('%', ''))
-
-    # Check if resizing is needed
-    if original_width < 512 or original_height < 512:
-        # Calculate scaling factors
-        scale_x = scale_y = max(512 / original_width, 512 / original_height)
-
-        # Resize the SVG
-        dwg = svgwrite.Drawing()
-        dwg.add(svgwrite.image.Image(input_svg, insert=(0, 0), size=(original_width * scale_x, original_height * scale_y)))
-        
-        # Save the resized SVG with the same filename
-        output_filename = f"resized_{input_svg}"
-        dwg.saveas(output_filename)
-        print(f"Resized SVG saved as {output_filename}")
-    else:
-        print("The SVG is already larger than 512x512")
-
 import svgutils.transform as sg
-import sys
+import os
 
-fig = sg.fromfile('myimage.svg')
-fig.set_size(('200','200'))
-fig.save('myimage2.svg')
 
-# Replace 'input.svg' with your file name
-resize_svg('map copy.svg')
+def resize_svg(filepath: str, size: tuple['str', 'str'] = ('512', '512'), temp_mode: bool = False) -> str:
+    directory = os.path.dirname(filepath)
+    input_filename = os.path.basename(filepath)
+    if temp_mode:
+        output_filename = "tmp_" + input_filename
+    else:
+        output_filename = "resized_" + input_filename
+
+    output_filepath = os.path.join(directory, output_filename)
+    
+    fig = sg.fromfile(filepath)
+    fig.set_size(size)
+    fig.save(output_filepath)
+
+resize_svg('myimage2.svg')
