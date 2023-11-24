e6a61e66581203a1ee2deb78e7f1dc5b423ecb8a
Shabab Noor <ashababnoor@gmail.com>
2023-11-24 23:29:20 +0600

    renamed resize file in svg<>jpg converter

---
+++ projects/tools/svg-to-jpg-converter/resize.py
@@ -0,0 +1,22 @@
+import svgutils.transform as sg
+import os
+
+
+def resize_svg(filepath: str, size: tuple['str', 'str'] = ('512', '512'), temp_mode: bool = False) -> str:
+    directory = os.path.dirname(filepath)
+    input_filename = os.path.basename(filepath)
+    if temp_mode:
+        output_filename = "tmp_" + input_filename
+    else:
+        output_filename = "resized_" + input_filename
+
+    output_filepath = os.path.join(directory, output_filename)
+    
+    fig = sg.fromfile(filepath)
+    fig.set_size(size)
+    fig.save(output_filepath)
+    return output_filepath
+
+
+if __name__ == "__main__":
+    resize_svg('myimage2.svg')
