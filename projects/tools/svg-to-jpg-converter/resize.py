c649377920429c679e54a578b97d264959bed052
Shabab Noor <ashababnoor@gmail.com>
2023-11-24 23:35:01 +0600

    added utils file with utility functions and updated resize file to make use of utils

--- projects/tools/svg-to-jpg-converter/resize.py
+++ projects/tools/svg-to-jpg-converter/resize.py
@@ -1,16 +1,14 @@
 import svgutils.transform as sg
-import os
+from utils import prefix_on_filename
 
 
 def resize_svg(filepath: str, size: tuple['str', 'str'] = ('512', '512'), temp_mode: bool = False) -> str:
-    directory = os.path.dirname(filepath)
-    input_filename = os.path.basename(filepath)
     if temp_mode:
-        output_filename = "tmp_" + input_filename
+        prefix = "tmp_"
     else:
-        output_filename = "resized_" + input_filename
+        prefix = "resized_"
 
-    output_filepath = os.path.join(directory, output_filename)
+    output_filepath = prefix_on_filename(filepath=filepath, prefix=prefix)
     
     fig = sg.fromfile(filepath)
     fig.set_size(size)
