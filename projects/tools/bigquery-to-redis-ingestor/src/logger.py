a5421ef4887d1ef4adbb5c5a643828f262683143
Shabab Noor <ashababnoor@gmail.com>
2023-12-02 02:34:50 +0600

    moved projects to other repositories; check README for location

--- projects/tools/bigquery-to-redis-ingestor/src/logger.py
+++
@@ -1,6 +0,0 @@
-from utilities.logger import get_logger
-
-logger = get_logger(
-    add_console_handler=True,
-    add_file_handler=False
-)
--- projects/tools/bigquery-to-redis-ingestor/src/utilities/__main__.py
+++
@@ -1,9 +0,0 @@
-from classes.color import Color
-from classes.print import Print
-from classes.codeblock import CodeBlock
-from classes.timerblock import TimerBlock
-from functions import *
-from lambdas import *
-
-
-print("Hello world!")
