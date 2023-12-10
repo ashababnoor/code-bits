0b2ce79743742728b5d2b07e699389de61dbb22a
Shabab Noor <ashababnoor@gmail.com>
2023-12-10 23:36:00 +0600

    added new options to be added as TODO in gitit script

--- shell-scripts/gitit.sh
+++ shell-scripts/gitit.sh
@@ -172,7 +172,7 @@ function git_add_commit_push() {
                 ;;
         esac
     done
-    
+
     # Check if a commit message is provided
     if [[ -z $commit_message ]]; then
         echo -e "${red_bold}Error:${reset} Please provide a commit message"
@@ -222,3 +222,16 @@ function git_add_commit_push() {
 alias gitit=git_add_commit_push
 alias gpush=do_git_push
 alias gpull=do_git_pull
+
+
+# TODO: New features to be added 
+
+# --amend: This option could be used to amend the last commit. This is useful if you made a mistake in your last commit message or forgot to add some changes.
+
+# --force: This option could be used to force push to the remote repository. This is useful when you need to overwrite the remote branch, but should be used with caution.
+
+# --branch: This option could allow the user to specify a branch to push to, instead of always pushing to the current branch.
+
+# --remote: This option could allow the user to specify a remote to push to, instead of always pushing to the default remote.
+
+# --all: This option could add all changes (including untracked files) to the staging area, instead of only changes to already tracked files.
