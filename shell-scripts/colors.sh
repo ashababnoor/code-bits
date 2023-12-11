e3917ad5fd563bafde0b634308d1335be2e65a65
Shabab Noor <ashababnoor@gmail.com>
2023-12-11 23:54:44 +0600

    updated colors.sh: updated variable names for color codes

--- shell-scripts/colors.sh
+++ shell-scripts/colors.sh
@@ -10,48 +10,48 @@ style_bold=$(echo -e '\033[1m')  # bold text
 style_reset=$(echo -e '\033[0m') # reset color and formatting
 
 # Define 3-bit non-bold color codes
-black=$(echo -e '\033[0;30m')         # Black
-red=$(echo -e '\033[0;31m')           # Red
-green=$(echo -e '\033[0;32m')         # Green
-yellow=$(echo -e '\033[0;33m')        # Yellow
-blue=$(echo -e '\033[0;34m')          # Blue
-magenta=$(echo -e '\033[0;35m')       # Magenta
-cyan=$(echo -e '\033[0;36m')          # Cyan
-white=$(echo -e '\033[0;37m')         # White
+color_black=$(echo -e '\033[0;30m')         # Black
+color_red=$(echo -e '\033[0;31m')           # Red
+color_green=$(echo -e '\033[0;32m')         # Green
+color_yellow=$(echo -e '\033[0;33m')        # Yellow
+color_blue=$(echo -e '\033[0;34m')          # Blue
+color_magenta=$(echo -e '\033[0;35m')       # Magenta
+color_cyan=$(echo -e '\033[0;36m')          # Cyan
+color_white=$(echo -e '\033[0;37m')         # White
 
 # Define 3-bit background codes
-black_bg=$(echo -e '\033[0;40m')      # Black Background
-red_bg=$(echo -e '\033[0;41m')        # Red Background
-green_bg=$(echo -e '\033[0;42m')      # Green Background
-yellow_bg=$(echo -e '\033[0;43m')     # Yellow Background
-blue_bg=$(echo -e '\033[0;44m')       # Blue Background
-magenta_bg=$(echo -e '\033[0;45m')    # Magenta Background
-cyan_bg=$(echo -e '\033[0;46m')       # Cyan Background
-white_bg=$(echo -e '\033[0;47m')      # White Background
+color_black_bg=$(echo -e '\033[0;40m')      # Black Background
+color_red_bg=$(echo -e '\033[0;41m')        # Red Background
+color_green_bg=$(echo -e '\033[0;42m')      # Green Background
+color_yellow_bg=$(echo -e '\033[0;43m')     # Yellow Background
+color_blue_bg=$(echo -e '\033[0;44m')       # Blue Background
+color_magenta_bg=$(echo -e '\033[0;45m')    # Magenta Background
+color_cyan_bg=$(echo -e '\033[0;46m')       # Cyan Background
+color_white_bg=$(echo -e '\033[0;47m')      # White Background
 
 # Define 3-bit bold color color codes
-black_bold=$(echo -e '\033[1;30m')    # Bold Black
-red_bold=$(echo -e '\033[1;31m')      # Bold Red
-green_bold=$(echo -e '\033[1;32m')    # Bold Green
-yellow_bold=$(echo -e '\033[1;33m')   # Bold Yellow
-blue_bold=$(echo -e '\033[1;34m')     # Bold Blue
-magenta_bold=$(echo -e '\033[1;35m')  # Bold Magenta
-cyan_bold=$(echo -e '\033[1;36m')     # Bold Cyan
-white_bold=$(echo -e '\033[1;37m')    # Bold White
+color_black_bold=$(echo -e '\033[1;30m')    # Bold Black
+color_red_bold=$(echo -e '\033[1;31m')      # Bold Red
+color_green_bold=$(echo -e '\033[1;32m')    # Bold Green
+color_yellow_bold=$(echo -e '\033[1;33m')   # Bold Yellow
+color_blue_bold=$(echo -e '\033[1;34m')     # Bold Blue
+color_magenta_bold=$(echo -e '\033[1;35m')  # Bold Magenta
+color_cyan_bold=$(echo -e '\033[1;36m')     # Bold Cyan
+color_white_bold=$(echo -e '\033[1;37m')    # Bold White
 
 # Define 8-bit color non-bold codes
-orage=$(echo -e '\033[38;5;214m')          # Orange
-dark_orange=$(echo -e '\033[38;5;208m')    # Dark Orange
-orange_red=$(echo -e '\033[38;5;202m')     # Orange Red
-light_sea_green=$(echo -e '\033[38;5;37m') # Light Sea Green
-dodger_blue=$(echo -e '\033[38;5;33m')     # Dodger Blue
+color_orage=$(echo -e '\033[38;5;214m')          # Orange
+color_dark_orange=$(echo -e '\033[38;5;208m')    # Dark Orange
+color_orange_red=$(echo -e '\033[38;5;202m')     # Orange Red
+color_light_sea_green=$(echo -e '\033[38;5;37m') # Light Sea Green
+color_dodger_blue=$(echo -e '\033[38;5;33m')     # Dodger Blue
 
 # Define 8-bit color bold codes
-orage_bold=$(echo -e '\033[1;38;5;214m')          # Bold Orange
-dark_orange_bold=$(echo -e '\033[1;38;5;208m')    # Bold Dark Orange
-orange_red_bold=$(echo -e '\033[1;38;5;202m')     # Bold Orange Red
-light_sea_green_bold=$(echo -e '\033[1;38;5;37m') # Bold Light Sea Green
-dodger_blue_bold=$(echo -e '\033[1;38;5;33m')     # Bold Dodger Blue
+color_orage_bold=$(echo -e '\033[1;38;5;214m')          # Bold Orange
+color_dark_orange_bold=$(echo -e '\033[1;38;5;208m')    # Bold Dark Orange
+color_orange_red_bold=$(echo -e '\033[1;38;5;202m')     # Bold Orange Red
+color_light_sea_green_bold=$(echo -e '\033[1;38;5;37m') # Bold Light Sea Green
+color_dodger_blue_bold=$(echo -e '\033[1;38;5;33m')     # Bold Dodger Blue
 
 # Read more: 
 #   1) https://en.wikipedia.org/wiki/ANSI_escape_code
