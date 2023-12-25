ff65f26086be8665049ccf3382a679742fd6fdd4
Shabab Noor <ashababnoor@gmail.com>
2023-12-25 23:35:30 +0600

    refactor (kill-port): renamed variables for clarity

--- shell-scripts/kill-process-on-port.sh
+++ shell-scripts/kill-process-on-port.sh
@@ -5,15 +5,15 @@ kill_process_on_port() {
     fi
 
     local port="$1"
-    local pid_list=$(lsof -t -i :"$port")
+    local process_ids=$(lsof -t -i :"$port")
 
-    if [[ -z $pid_list ]]; then
+    if [[ -z $process_ids ]]; then
         echo "No processes found running on port $port"
     else
         echo "Processes running on port $port:"
-        echo "$pid_list"
+        echo "$process_ids"
         echo -en "Killing processes... "
-        kill -9 $pid_list
+        kill -9 $process_ids
         echo "Done"
     fi
 }
