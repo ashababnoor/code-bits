5f7b87fe3f8bd24087612e9d1761e8cc54db3e2e
Shabab Noor <ashababnoor@gmail.com>
2024-01-10 23:36:06 +0600

    docs: added problem desciption for monty hall problem in simulation script

--- simulations/monte-hall-problem.py
+++ simulations/monte-hall-problem.py
@@ -1,3 +1,23 @@
+'''
+This script runs a simulation on the Monty Hall problem.
+
+Problem description:
+The Monty Hall problem is a probability puzzle based on a game show scenario. 
+In the game, there are three doors, behind one of which is a valuable prize, 
+and behind the other two are goats. The contestant initially chooses one door. 
+After the contestant makes their choice, the host, who knows what is behind 
+each door, opens one of the remaining doors to reveal a goat. The contestant is 
+then given the option to switch their choice to the other unopened door or stick 
+with their original choice. The question is: What is the optimal strategy for 
+the contestant to maximize their chances of winning the prize?
+
+This script simulates the Monty Hall problem by running multiple trials. 
+In each trial, the script randomly selects a door for the prize, the contestant's 
+initial choice, and the door to be revealed by the host. It then calculates the 
+probabilities of winning with and without switching the chosen door. The simulation 
+is run for a specified number of trials and the resulting probabilities are printed.
+'''
+
 import random
 
 def monte_hall_simulation(num_trials):
